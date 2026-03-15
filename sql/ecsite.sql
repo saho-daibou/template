d4212615889b01b38a1de735a45e7014e6324b41
saho <jyako.t@gmail.com>
2026-03-15 23:04:36 +0900

    スペルミス修正

--- sql/ecsite.sql
+++ sql/ecsite.sql
@@ -12,7 +12,7 @@ create table login_user_transaction(
 	login_pass varchar(16),
 	user_name varchar(50),
 	insert_date datetime,
-	updated_date datetime
+	update_date datetime
 );
 
 drop table if exists item_info_transaction;
@@ -38,8 +38,6 @@ create table user_buy_item_transaction(
 	delete_date datetime
 );
 
-INSERT INTO item_info_transaction(item_name, item_price, item_stock) VALUES("ノートBook", 100,
-50);
+INSERT INTO item_info_transaction(item_name, item_price, item_stock) VALUES("ノートBook", 100,50);
 
-INSERT INTO login_user_transaction(login_id, login_pass, user_name) VALUES("internous",
-"internous01", "test");
+INSERT INTO login_user_transaction(login_id, login_pass, user_name) VALUES("internous","internous01", "test");
